4b85f9ad532f8ab4691fd5b9ad76d803e6fb1f72
pratikdange999 <pratikdange999@gmail.com>
2021-10-12 20:35:37 +0530

    commit on 'main'

--- 02. Flow Control, Functions & Loops/28_Next_Prime_Number.cpp
+++ 02. Flow Control, Functions & Loops/28_Next_Prime_Number.cpp
@@ -1,56 +1,24 @@
-// C++ implementation of the approach (GFG Reference)
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
- 
-// Function that returns true if n
-// is prime else returns false
-bool isPrime(int n)
+int nextPrime(int n)
 {
-    // Corner cases
-    if (n <= 1)  return false;
-    if (n <= 3)  return true;
-   
-    // This is checked so that we can skip 
-    // middle five numbers in below loop
-    if (n%2 == 0 || n%3 == 0) return false;
-   
-    for (int i=5; i*i<=n; i=i+6)
-        if (n%i == 0 || n%(i+2) == 0)
-           return false;
-   
-    return true;
-}
- 
-// Function to return the smallest
-// prime number greater than N
-int nextPrime(int N)
-{
- 
-    // Base case
-    if (N <= 1)
-        return 2;
- 
-    int prime = N;
-    bool found = false;
- 
-    // Loop continuously until isPrime returns
-    // true for a number greater than n
-    while (!found) {
-        prime++;
- 
-        if (isPrime(prime))
-            found = true;
+    n++;
+    while(true)
+    {
+        int i;
+        for(i=2;i<n;i++)
+        {
+            if(n%i==0)
+            break;
+        }
+        if(i==n) return n;
+        n++;    
     }
- 
-    return prime;
 }
- 
-// Driver code
 int main()
 {
-    int N = 3;
- 
-    cout << nextPrime(N);
- 
+    int x;
+    cin>>x;
+    cout<<nextPrime(x);
     return 0;
 }
